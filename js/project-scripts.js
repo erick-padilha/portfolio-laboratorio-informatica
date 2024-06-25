744544a9a8e9e9ea47cb7fd2d9dda90d80352f24
Erick Padilha <erickpadilha75@gmail.com>
2024-06-25 16:37:31 -0300

    Adicionando js para alterar as imagens dos cards

---
+++ js/project-scripts.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const imageSets = [
+    ["img/image1.jpg", "img/image1-2.jpg", "img/image1-3.jpg"],
+    ["img/image2.jpg", "img/image2-2.jpg", "img/image2-3.jpg"],
+    ["img/image3.jpg", "img/image3-2.jpg", "img/image3-3.jpg"],
+    ["img/image4.jpg", "img/image4-2.jpg", "img/image4-3.jpg"],
+  ];
+
+  const cards = document.querySelectorAll(".project-image");
+
+  cards.forEach((card, index) => {
+    let currentIndex = 0;
+    setInterval(() => {
+      currentIndex = (currentIndex + 1) % imageSets[index].length;
+      card.src = imageSets[index][currentIndex];
+    }, 5000);
+  });
+});
