0ae3aca7a3412f5f9309c8e11622c8c78f87fed7
Erick Padilha <erickpadilha75@gmail.com>
2024-06-28 16:57:31 -0300

    Alterando as páginas com estilização e bootstrap

--- js/project-scripts.js
+++ js/project-scripts.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const imageSets = [
         ["img/image1_card1.jpg", "img/image2_card1.jpg", "img/image3_card1.jpg", "img/image4_card1.jpg", "img/image5_card1.jpg"],
-        ["img/image2.jpg", "img/image2-2.jpg", "img/image2-3.jpg"],
-        ["img/image3.jpg", "img/image3-2.jpg", "img/image3-3.jpg"],
-        ["img/image4.jpg", "img/image4-2.jpg", "img/image4-3.jpg"]
+        ["img/image1_card1.jpg", "img/image2_card1.jpg", "img/image3_card1.jpg", "img/image4_card1.jpg", "img/image5_card1.jpg"],
+        ["img/image1_card1.jpg", "img/image2_card1.jpg", "img/image3_card1.jpg", "img/image4_card1.jpg", "img/image5_card1.jpg"],
+        ["img/image1_card1.jpg", "img/image2_card1.jpg", "img/image3_card1.jpg", "img/image4_card1.jpg", "img/image5_card1.jpg"],
+        ["img/image1_card1.jpg", "img/image2_card1.jpg", "img/image3_card1.jpg", "img/image4_card1.jpg", "img/image5_card1.jpg"]
     ];
 
     const cards = document.querySelectorAll('.project-image');
