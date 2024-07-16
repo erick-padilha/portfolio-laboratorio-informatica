76eac57dc2b1b8cb012b2414a17b8d1f284666ae
Erick Padilha <erickpadilha75@gmail.com>
2024-07-16 16:54:07 -0300

    Alterando a lógica das cartas e a página

--- js/carta-script.js
+++ js/carta-script.js
@@ -1,11 +1,25 @@
-document.getElementById('printForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const password = document.getElementById('password').value;
-    const correctPassword = '1234abc'; // Substitua 'sua_senha' pela senha correta
+function visualizarCarta(caminho) {
+  window.open(caminho, "_blank");
+}
 
-    if (password === correctPassword) {
-        window.print();
-    } else {
-        alert('Senha incorreta!');
-    }
-});
+function baixarCarta(caminho) {
+  $("#passwordModal").modal("show");
+
+  document
+    .getElementById("passwordForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      const password = document.getElementById("password").value;
+      const correctPassword = "1234"; // Substitua 'sua_senha' pela senha correta
+
+      if (password === correctPassword) {
+        const link = document.createElement("a");
+        link.href = caminho;
+        link.download = caminho.split("/").pop();
+        link.click();
+        $("#passwordModal").modal("hide");
+      } else {
+        alert("Senha incorreta!");
+      }
+    });
+}
