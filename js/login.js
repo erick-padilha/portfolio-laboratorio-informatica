5663862a8cfa0980f334d44317779e63a31e8cfc
Erick Padilha <erickpadilha75@gmail.com>
2024-07-26 23:46:51 -0300

    Adicionando as senhas ao projeto

--- js/login.js
+++ js/login.js
@@ -6,10 +6,32 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
 
     // Armazenamento simples de credenciais (substitua isso por uma lógica de armazenamento seguro)
     var users = {
-        "user1": "password1",
-        "user2": "password2",
-        "sv77@calabria.com.br": "SU1873porte@",
-        "educando": "senha123"
+        "Arthur": "Art12345",
+        "Joao": "Joao2024",
+        "Luan": "Luan5678",
+        "Luka": "Luka1234",
+        "Nicolle": "Nic2024",
+        "Ryan": "Ryan5678",
+        "Sarah": "Sar12345",
+        "Simeao": "Sim12345",
+        "Shayera": "Sha5678",
+        "Brendo": "Bre2024",
+        "Emilly": "Emi5678",
+        "Erick": "Eri12345",
+        "Erika": "Eri2024",
+        "Frank": "Fra5678",
+        "Gionava": "Gio12345",
+        "Guilherme": "Gui2024",
+        "Isadora": "Isa5678",
+        "Luciano": "Luc12345",
+        "Luigi": "Lui2024",
+        "Miguel": "Mig5678",
+        "Rai": "Rai12345",
+        "Ruti": "Rut2024",
+        "Sthefany": "Sth5678",
+        "Victorya": "Vic12345",
+        "Yuri": "Yur2024",
+        "Educador": "TEC@1873"
         // Adicione mais usuários e senhas conforme necessário
     };
 
